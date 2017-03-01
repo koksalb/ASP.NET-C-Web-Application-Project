f3ff45331b746d2cf28ccb9f5bb5d3ecbddad2c9
koksalb <koksalb@itu.edu.tr>
2017-03-01 15:29:58 +0300

    First SQL Query from C#
    First SQL query attempts are successfull
    The c# file of the placespage can now send queries to connection strings

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class _Default : Page
 {
--- PlacesPage.aspx.cs
+++ PlacesPage.aspx.cs
@@ -4,11 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class PlacesPage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+
+        //if (IsPostBack)
+      //  {
+
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RestaurantsConnectionString"].ConnectionString);
+            
+            conn.Open();
+
+            string testquery = "select count(*) from Restaurants where Days_Since_Last_Visit > 3";
+
+            SqlCommand com =new SqlCommand(testquery,conn);
+            int result = Convert.ToInt32(com.ExecuteScalar().ToString());
+
+            if (result > 0)
+            {
+                Response.Write("I Found some restaurants!");
+            }
+            else
+            {
+                Response.Write("I have nothing :(");
+            }
+
+            conn.Close();
+
+       // }
+
 
     }
 }
