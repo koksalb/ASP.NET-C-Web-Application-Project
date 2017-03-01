8a32bdeff2a9880224241a657916a57cff02560f
koksalb <koksalb@itu.edu.tr>
2017-03-01 12:40:26 +0300

    initial template asp.net project
    Default / Contact / About pages included template vs project

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(project1.Startup))]
+namespace project1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
