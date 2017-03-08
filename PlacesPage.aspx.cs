364823b6bb1a7d4aba70bc1201d5adb39bdb415f
koksalb <koksalb@itu.edu.tr>
2017-03-08 20:30:08 +0300

    Object Classes are added
    Restaurant Class is added
    Basic SQL query to select restaurant is written
    
    Requirements are set to be: haven't visited yesterday, didnt reach
    monthly limit

--- PlacesPage.aspx.cs
+++ PlacesPage.aspx.cs
@@ -9,6 +9,36 @@ using System.Configuration;
 
 public partial class PlacesPage : System.Web.UI.Page
 {
+
+    public class restaurant
+    {
+
+        public int Id {get;set;}
+        
+        public String Place_Name{get;set;}
+
+        public Boolean Affectedfromweather{get;set;}
+
+        public DateTime Latest_Visit_Date{get;set;}
+
+        public int Total_Visits_This_Month{get;set;}
+
+        public Boolean CarorWalk{get;set;}
+
+        public int Total_People_Voted{get;set;}
+
+        public int Total_Votes{get;set;}
+
+        public float Average_Vote{get;set;}
+
+        public int Expected_Visits_This_Month{get;set;}
+
+        public int Days_Since_Last_Visit { get; set; }
+
+    }
+
+
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -20,19 +50,48 @@ public partial class PlacesPage : System.Web.UI.Page
             
             conn.Open();
 
-            string testquery = "select count(*) from Restaurants where Days_Since_Last_Visit > 3";
+            string testquery = "select * from Restaurants" +
+                
+                                " where "+
+
+                                "Days_Since_Last_Visit > 1"+
+
+                                " and "+
+
+                                "(Expected_Visits_This_Month-Total_Visits_This_Month) >= 0"
+                
+                
+                ;
 
             SqlCommand com =new SqlCommand(testquery,conn);
-            int result = Convert.ToInt32(com.ExecuteScalar().ToString());
 
-            if (result > 0)
+            var list = new List<restaurant>();
+
+            using(var command = com.ExecuteReader())
             {
-                Response.Write("I Found some restaurants!");
+                while(command.Read())
+                {
+                    /*list.Add(new restaurant {
+                        Id = command.GetInt32(0),
+                        Place_Name = command.GetString(1),
+                        Affectedfromweather = command.GetBoolean(2),
+                        Latest_Visit_Date = command.GetDateTime(3),
+                        Total_Visits_This_Month = command.GetInt32(4),
+                        CarorWalk = command.GetBoolean(5),
+                        Total_People_Voted = command.GetInt32(6),
+                        Total_Votes=command.GetInt32(7),
+                        Average_Vote=command.GetInt32(8),
+                        Days_Since_Last_Visit=command.GetInt32(9),
+                        Expected_Visits_This_Month=command.GetInt32(10)
+                    });
+                     * 
+                     * */
+
+                    string foundplacename =  command.GetString(1);
+                    Response.Write("I Found " + foundplacename + " for you !");
+                }
             }
-            else
-            {
-                Response.Write("I have nothing :(");
-            }
+
 
             conn.Close();
 
