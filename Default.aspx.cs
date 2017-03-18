93fe14b3b5f30c3b7eb688621fe194cf19a911be
koksalb <koksalb@itu.edu.tr>
2017-03-18 23:08:00 +0300

    Database Extension
    The database is added with more restaurants
    The list of restaurants are taken in the runtime to be able to use C# to
    manipulate for later

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -13,7 +13,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Web.UI.HtmlControls;
-
+using System.Text.RegularExpressions;
 
 public partial class _Default : Page
 {
@@ -74,18 +74,7 @@ public partial class _Default : Page
 
         SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RestaurantsConnectionString"].ConnectionString);
         conn.Open();
-        string testquery = "select * from Restaurants" +
-
-                            " where " +
-
-                            "Days_Since_Last_Visit > 1" +
-
-                            " and " +
-
-                            "(Expected_Visits_This_Month-Total_Visits_This_Month) >= 0"
-
-
-            ;
+        string testquery = "select * from Restaurants";
 
         SqlCommand com = new SqlCommand(testquery, conn);
         using (var command = com.ExecuteReader())
@@ -111,9 +100,11 @@ public partial class _Default : Page
         }
         conn.Close();
 
+        UpdateDatabaseWithTheList(list);
 
 
     }
+
 
     protected void UpdateDatabaseWithTheList(List<restaurant> liste)
     {
@@ -123,7 +114,54 @@ public partial class _Default : Page
         string testquery = "delete from Restaurants where id=id";
         SqlCommand com = new SqlCommand(testquery, conn);
         var command = com.ExecuteReader();
-        GridView1.DataBind();
+        conn.Close();
+        for (int i = 0; i < list.Count;i++ )
+        {
+            SqlConnection conn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["RestaurantsConnectionString"].ConnectionString);
+            conn2.Open();
+            string testquery2 = "insert into Restaurants (Id,Place_Name,Affectedfromweather,Latest_Visit_Date,Total_Visits_This_Month,CarorWalk,Total_People_Voted,Total_Votes,Average_Vote,Expected_Visits_This_Month,Days_Since_Last_Visit) VALUES ("
+
+                +
+            list.ElementAt(i).Id + "," +
+             "'" + Regex.Replace(list.ElementAt(i).Place_Name, @"\s", "") + "'" + ",";
+            if (list.ElementAt(i).Affectedfromweather)
+            { 
+            testquery2 = testquery2 + "1"+",";
+            }
+            else 
+            {
+            testquery2 = testquery2 + "0" + ",";
+            }
+
+            string format = "yyyy-MM-dd HH:mm:ss";
+            testquery2 +=
+              "'" + list.ElementAt(i).Latest_Visit_Date.ToString(format) + "'" + "," +
+              list.ElementAt(i).Total_Visits_This_Month + ",";
+
+            if (list.ElementAt(i).CarorWalk)
+            {
+                testquery2 = testquery2 + "1" + ",";
+            }
+            else
+            {
+                testquery2 = testquery2 + "0" + ",";
+            }
+
+            
+            testquery2 +=
+                  list.ElementAt(i).Total_People_Voted+","+
+                   list.ElementAt(i).Total_Votes+","+
+                   Regex.Replace( list.ElementAt(i).Average_Vote.ToString(), @",", ".")+","+
+                     list.ElementAt(i).Expected_Visits_This_Month+","+
+                      list.ElementAt(i).Days_Since_Last_Visit+");"
+                     
+                ;
+            SqlCommand com2 = new SqlCommand(testquery2, conn2);
+            var command2 = com2.ExecuteReader();
+            conn2.Close();
+
+        }
+            GridView1.DataBind();
             
 
 
